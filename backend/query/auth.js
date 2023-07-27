39738201b6e69ad64010467421f48adae5cf08de
Thuận Nguyễn Chí <nguyenchithuan55@gmail.com>
2023-07-27 09:12:22 +0700

    Update

---
+++ backend/query/auth.js
@@ -0,0 +1,19 @@
+var config = require('../config/dbconfig');
+const sql = require('mssql');
+async function getOrders(req) {
+    try {
+        let pool = await sql.connect(config);
+        let user = await pool.request().query(`SELECT * from USER_INFO where username='${req.body.username}' and pass='${req.body.password}'`);
+        if(user.recordset.length==1)
+        {
+            return 'success'
+        }
+        else return 'fail'
+    }
+    catch (error) {
+        return error
+    }
+}
+module.exports = {
+    getOrders: getOrders
+}
