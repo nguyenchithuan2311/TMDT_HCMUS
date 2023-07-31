5cd0bde83c1101ffc96ca14910499cb4b025a1a2
vlathong <105715507+vlathong@users.noreply.github.com>
2023-07-31 11:17:55 +0700

    gg2

---
+++ frontend/src/page/Footer.jsx
@@ -0,0 +1,17 @@
+import "../App.css";
+
+
+export const Footer = () => {
+    return (
+        <div class="footer">
+            <div className="t-footer">
+            <span class="text1">© 2023 Copyright: </span>
+            <span class="text2">MDBootstrap.com</span>
+            </div>
+            
+        </div>
+    )
+
+}
+
+export default Footer;
