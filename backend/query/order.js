b46136b0ed15985e1c05fb545030d3f161ad00cb
TuanKiet297 <tuankietnguyen275@gmail.com>
2023-08-07 16:31:29 +0700

    modified:   backend/query/order.js modified:   backend/router/order.js

--- backend/query/order.js
+++ backend/query/order.js
@@ -1,6 +1,6 @@
 var config = require('../config/dbconfig');
 const sql = require('mssql');
-async function getOrders(req) {
+async function getOrders() {
     try {
         let pool = await sql.connect(config);
         let order = await pool.request().query(`SELECT * from ORDER_DETAILS`);
@@ -10,6 +10,75 @@ async function getOrders(req) {
         return error
     }
 }
+async function getUOrder(req) {
+    try {
+        let pool = await sql.connect(config);
+        let order = await pool.request()
+        .input('ID',sql.Char(5), req)
+        .query(`SELECT * from ORDER_DETAILS where @ID = ID`);
+        return order.recordset
+    }
+    catch (error) {
+        return error
+    }
+}
+async function createOrders(req) {
+    try {
+        let pool = await sql.connect(config);
+        let order = await pool.request()
+        .input('ID', sql.Char(5), req.ID)
+        .input('USER_ID', sql.Char(5), req.USER_ID)
+        .input('TOTAL', sql.Decimal, req.TOTAL)
+        .input('POINT', sql.Int, req.POINT)
+        .input('PAYMENT_ID', sql.Char(5), req.PAYMENT_ID)
+        .input('STATUS', sql.NChar(20), req.STATUS)
+        .input('CREATED_AT', sql.DateTime, req.CREATED_AT)
+        .input('MODIFIED_AT', sql.DateTime, req.MODIFIED_AT)
+        .query(`INSERT INTO ORDER_DETAILS VALUES(@ID,@USER_ID,@TOTAL,@POINT,@PAYMENT_ID,@STATUS,@CREATED_AT,@MODIFIED_AT);`);
+        return "Successful"
+    }
+    catch (error) {
+        return error
+    }
+}
+async function updateOrders(req) {
+    try {
+        let pool = await sql.connect(config);
+        let order = await pool.request()
+        .input('ID', sql.Char(5), req.ID)
+        .input('USER_ID', sql.Char(5), req.USER_ID)
+        .input('TOTAL', sql.Decimal, req.TOTAL)
+        .input('POINT', sql.Int, req.POINT)
+        .input('PAYMENT_ID', sql.Char(5), req.PAYMENT_ID)
+        .input('STATUS', sql.NChar(20), req.STATUS)
+        .input('CREATED_AT', sql.DateTime, req.CREATED_AT)
+        .input('MODIFIED_AT', sql.DateTime, req.MODIFIED_AT)
+        .query(`UPDATE ORDER_DETAILS SET USER_ID = @USER_ID, TOTAL = @TOTAL,
+                POINT = @POINT,PAYMENT_ID = @PAYMENT_ID, STATUS = @STATUS, CREATED_AT = @CREATED_AT, MODIFIED_AT = @MODIFIED_AT
+                WHERE ID = @ID`);
+        return "Successful"
+    }
+    catch (error) {
+        return error
+    }
+}
+async function deleteOrders(req) {
+    try {
+        let pool = await sql.connect(config);
+        let order = await pool.request()
+        .input('ID', sql.Char(5), req)
+        .query(`DELETE FROM ORDER_DETAILS
+                WHERE ID = @ID;`);
+        return "Successful"
+    }
+    catch (error) {
+        return error
+    }
+}
 module.exports = {
-    getOrders: getOrders
+    getOrders: getOrders,
+    getUOrder: getUOrder,
+    createOrders: createOrders,
+    updateOrders: updateOrders,
+    deleteOrders: deleteOrders
 }
