12a3886091f330c34b3ffd42b8b4e78d99998b66
Nguyễn Tuấn Kiệt <102739504+TuanKiet297@users.noreply.github.com>
2023-08-10 15:32:18 +0700

    Update dbconfig.js

--- backend/config/dbconfig.js
+++ backend/config/dbconfig.js
@@ -1,7 +1,7 @@
 const config = {
-    user :'admin',
-    password :'admin',
-    server:'LAPTOP-II48F2VK',
+    user :'sa',
+    password :'thuan2311',
+    server:'LAPTOP-H6Q2P9SU'
     database:'QL_BANHANG',
     options:{
         encrypt: true,
