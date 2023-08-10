caa143b77d81c9476b4484c758107e52a3554e8c
TuanKiet297 <tuankietnguyen275@gmail.com>
2023-08-10 15:27:24 +0700

    modified:   backend/config/dbconfig.js modified:   backend/index.js modified:   backend/query/order.js new file:   backend/query/product.js new file:   backend/router/product.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const auth = require("./router/auth");
 const order = require("./router/order");
 const user = require("./router/user");
+const user = require("./router/product");
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -11,5 +12,6 @@ app.use(bodyParser.json());
 app.use('/api', auth);
 app.use('/order', order);
 app.use('/user', user);
+app.use('/product', product);
 
 var server = app.listen(3000, () => console.log(`Server is starting at port ${3000}`));
